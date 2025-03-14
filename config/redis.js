934a1bc377aba38e1f925d81ecc6ba3296feacac
Firoz Ahammad <firozahamed0@gmail.com>
2025-03-14 15:50:47 +0600

    Radish chage

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 const redis = require('redis');
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URI,
+  url: process.env.REDIS_URL,
 });
 
 redisClient.on('error', (err) => console.error('Redis error:', err));
